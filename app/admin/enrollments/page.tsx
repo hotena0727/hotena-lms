a02f7a8a379b77a7bb68b924a3627d36a6c85dea
이선우 <hotena0727@gmail.com>
2026-03-14 21:06:17 +0900

    Fix member enrollment panel relation typing

--- app/admin/enrollments/page.tsx
+++ app/admin/enrollments/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
